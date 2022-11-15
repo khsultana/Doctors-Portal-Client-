3ff81e4209d56c7e44ce24ce217f589cf380d8db
Khan Sultana <Khan.Sultana@fifo-tech.com>
2022-11-15 17:48:29 +0600

    first commit

---
+++ src/Routes/Routes/Routes.js
@@ -0,0 +1,22 @@
+import Home from "../../Pages/Home/Home/Home";
+import Login from "../../Pages/Home/Login/Login";
+
+const { createBrowserRouter } = require("react-router-dom");
+const { default: Main } = require("../../Layout/Main");
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        children: [
+            {
+                path: "/",
+                element: <Home></Home>
+            },
+            {
+                path: "/login",
+                element: <Login></Login>
+            }
+        ]
+    }
+])
